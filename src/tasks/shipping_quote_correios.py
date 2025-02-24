60741175c8895421dc126d5f4072dbc22b9e7da3
Ghislaine Latorraca (Lena) <glenox79@gmail.com>
2025-02-24 14:56:35 -0300

    feat: Implement initial - Correios shipping quote functionality

--- src/tasks/shipping_quote_correios.py
+++ src/tasks/shipping_quote_correios.py
@@ -1,4 +1,4 @@
-from botcity.web import WebBot
+from botcity.web import WebBot, By
 import logging
 from config import *
 
@@ -8,3 +8,33 @@ def open_correios_site():
   bot.browse(URL_CORREIOS)
   bot.wait(3)
   logging.info("Abre site dos correios no navegador.")
+
+# DADOS PROVISORIOS PARA TESTE
+origin_zip_code = '38182-428'
+destination_zip_code = '74013-020'
+type_service = 'PAC'
+dimension_height = 36
+dimension_width = 28
+dimension_length = 28
+product_weight = 8
+
+def fill_correios_form(bot):
+  try:
+    logging.info("Preenchendo formulário no site dos correios...")
+
+    bot.find_element(selector="cepOrigem", by=By.NAME).send_keys(origin_zip_code)
+    bot.find_element(selector="cepDestino", by=By.NAME).send_keys(destination_zip_code)
+    bot.find_element(selector="servico", by=By.NAME).click()
+    bot.find_element(selector="option[value='04510']", by=By.CSS_SELECTOR).click()
+    bot.find_element(selector="option[value='outraEmbalagem1']", by=By.CSS_SELECTOR).click()
+    bot.find_element(selector="Altura", by=By.NAME).send_keys(str(dimension_height))
+    bot.find_element(selector="Largura", by=By.NAME).send_keys(str(dimension_width))
+    bot.find_element(selector="Comprimento", by=By.NAME).send_keys(str(dimension_length))
+    bot.find_element(selector="peso", by=By.NAME).click()
+    bot.find_element(selector="option[value='8']", by=By.CSS_SELECTOR).click()
+    bot.find_element(selector="Calcular", by=By.NAME).click()
+
+    logging.info("Formulário de cotação preenchido.")
+
+  except Exception as e:
+    logging.error(f"Erro no preenchimento do formulário de cotação dos correios: {e}")
