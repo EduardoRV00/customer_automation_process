f31bab4b30a19cb87c03d87d0c8013ee7463b0f4
GuiSartori <guilherme.sartori@live.com>
2025-03-10 09:45:49 -0300

    bot.py comments update

--- bot.py
+++ bot.py
@@ -1,24 +1,8 @@
 """
 WARNING:
-
 Please make sure you install the bot dependencies with `pip install --upgrade -r requirements.txt`
 in order to get all the dependencies on your Python environment.
 
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the dependencies.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install --upgrade -r requirements.txt`
-- Use the same interpreter as the one used to install the bot (`pip install --upgrade -r requirements.txt`)
-
-Please refer to the documentation for more information at
-https://documentation.botcity.dev/tutorials/python-automations/web/
 """
 
 import logging
@@ -40,14 +24,13 @@ BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
 
 def main():
+    
     try:
         logger_client, logger_dev = setup_logging()
         logger_client.info("Início da execução do processo.")
         logger_dev.info("Processo iniciado pelo bot.")
-        # Runner passes the server url, the id of the task being executed,
-        # the access token and the parameters that this task receives (when applicable).
+
         maestro = BotMaestroSDK.from_sys_args()
-        ## Fetch the BotExecution with details from the task, including parameters
         execution = maestro.get_execution()
 
         print(f"Task ID is: {execution.task_id}")
@@ -58,14 +41,8 @@ def main():
         # Configure whether or not to run on headless mode
         bot.headless = False
 
-        # Uncomment to set the WebDriver path
+        # Webdriver setting path
         bot.driver_path = CHROME_DRIVER
-
-        # Opens the BotCity website.
-        # open_correios_site()
-        # get_screenshots()
-
-        # Implement here your logic...
     
         # Creates the output sheet and assigns the file path to the variable output_sheet
         output_sheet = create_output_sheet()
@@ -73,18 +50,17 @@ def main():
         process_spreadsheet(output_sheet, logger_client, logger_dev)
         
         # Fill output_sheet with API consultation data
-        data_fill_processed(output_sheet)
+        data_fill_processed(output_sheet, logger_client, logger_dev)
         
-        # Fills in empty output sheet cells
         fill_data_b4_rpachallenge(output_sheet)
 
-        #Fill Rpa challenge text boxes.
-        open_rpa_challenge_website(bot)
+        # Fill Rpa challenge text boxes.
+        open_rpa_challenge_website(bot, logger_client, logger_dev)
         get_screenshots(logger_client, logger_dev)
         
-        fill_rpa_challenge(bot, output_sheet)
+        fill_rpa_challenge(bot, output_sheet, logger_client, logger_dev)
 
-        #performs quote on the correios website
+        # Performs quote on the correios website
         open_correios_site(bot, logger_client, logger_dev)
         logger_client.info("Inicia busca de cotação dos Correios.")
         get_screenshots(logger_client, logger_dev)
@@ -93,15 +69,11 @@ def main():
         get_screenshots(logger_client, logger_dev)
         logger_client.info("Fecha site dos correios no navegador.")
         bot.stop_browser()
-
         
-        # Check the output sheet information | Is currently running with placeholders
-        # validar_informacoes(quote_data)
         
-        # Performs quote on the jadlog website
+        # Opens jadlog site and performs quote
         open_jadlog_site(bot, logger_client, logger_dev)
         get_screenshots(logger_client, logger_dev)
-        # Performs quote on the jadlog website
         jadlog_quote(output_sheet, bot, logger_client, logger_dev)
         get_screenshots(logger_client, logger_dev)
         
@@ -110,17 +82,13 @@ def main():
 
         logger_client.info('Finalizando execução do bot...')
         logger_client.info("Processo Finalizado.")
-        # bot.wait(3000)
 
-        # Finish and clean up the Web Browser
-        # You MUST invoke the stop_browser to avoid
-        # leaving instances of the webdriver open
+        # Leaving instances of the webdriver open
         bot.stop_browser()
         subject = email_success_subject()
         body = email_success_body()
         send_email(output_sheet, subject, body, logger_client, logger_dev)
 
-        # Uncomment to mark this task as finished on BotMaestro
         # maestro.finish_task(
         #     task_id=execution.task_id,
         #     status=AutomationTaskFinishStatus.SUCCESS,
@@ -134,9 +102,7 @@ def main():
     except Exception as e:
         # Log the error and call the error handling function
         logger_dev.error(f"Erro fatal durante a execução do bot: {e}")
-        logging.info("Finalizando a execução do bot")
         handle_error("Execução da automação completa", "Erro durante a execução do bot(Execução geral do bot)", logger_client, logger_dev)
-
 
 
 def not_found(label):
